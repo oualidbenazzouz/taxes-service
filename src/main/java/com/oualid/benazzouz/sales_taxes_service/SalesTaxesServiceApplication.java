5f150eb47116ba03e8a39c0966b8c797e63b898f
Oualid Benazzouz <oualid.benazzouz1@gmail.com>
2024-12-08 18:04:10 +0100

    Initial commit: Spring Boot setup with Swagger and base project structure

---
+++ src/main/java/com/oualid/benazzouz/sales_taxes_service/SalesTaxesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oualid.benazzouz.sales_taxes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTaxesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesTaxesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oualid/benazzouz/sales_taxes_service/SalesTaxesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oualid.benazzouz.sales_taxes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTaxesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
